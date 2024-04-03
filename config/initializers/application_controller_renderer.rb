bbcaf4a92deacd33561311fa7e9db0c066f2b141
EC2 Default User <ec2-user@ip-172-31-33-184.ap-northeast-1.compute.internal>
2024-04-03 16:03:27 +0000

    for Heroku

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'a2f622cc64476f854f3b001adb5dea29cef8c6806b12960a91ed55ee80fa5cb942522d651cce2a798f179e4ccfac9993ee34f1cb7a797519380d394c52f29d71'
